abb8b81100ae9e9b4a7694ba1b2c06afe78f25e9
minju <minju@192.168.219.110>
2025-03-09 18:21:04 +0900

    javafx test

--- dwdwd/src/Namu.java
+++ dwdwd/src/Namu.java
@@ -1,8 +1,25 @@
-public class Namu {
-  public static void main(String[] args) {
-    printNamu();
-  }
-  public static void printNamu() {
-    System.out.println("Namu Wiki");
-  }
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Namu extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        Button btn = new Button("Hello, JavaFX!");
+        btn.setOnAction(e -> System.out.println("Button Clicked!"));
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+
+        Scene scene = new Scene(root, 300, 200);
+        primaryStage.setTitle("JavaFX Example");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
