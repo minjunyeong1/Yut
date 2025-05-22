27213c63f3658a892586214dd350b877d622bf49
ARanker <129069246+ARanker@users.noreply.github.com>
2025-05-22 15:28:52 +0900

    게임 승리시 재시작/종료 여부 선택

--- dwdwd/src/yutgame/controller/GameController.java
+++ dwdwd/src/yutgame/controller/GameController.java
@@ -1,11 +1,11 @@
-package yutgame.controller;
+package controller;
 
-import yutgame.model.Cell;
-import yutgame.model.GameModel;
-import yutgame.model.Piece;
-import yutgame.model.Player;
-import yutgame.model.YutThrowResult;
-import yutgame.view.*;
+import model.Cell;
+import model.GameModel;
+import model.Piece;
+import model.Player;
+import model.YutThrowResult;
+import view.*;
 
 import javax.swing.*;
 import java.util.List;
@@ -46,6 +46,18 @@ public class GameController {
 
             int steps = result.getValue();
 
+         // 1. 빽도인데 모든 말이 0번 셀에 있으면 → 턴 넘김
+            if (steps == -1 && moveTarget.getPosition().getId() == 0) {
+                boolean allAtStartCell = model.getCurrentPlayer().getPieces().stream()
+                    .allMatch(p -> p.getPosition() != null && p.getPosition().getId() == 0);
+
+                if (allAtStartCell) {
+                    model.getCurrentPlayer().getYutHistory().remove(result);
+                    nextTurn();
+                    return;
+                }
+            }
+
             // 2. 시작 셀(1번)에서 빽도일 경우 → 마지막 셀로 이동
             if (steps == -1 && moveTarget.getPosition() != null && moveTarget.getPosition().getId() == 1) {
                 Cell last = model.getBoard().getLastCell();  // Board 구현체에 getLastCell() 필요
@@ -74,12 +86,24 @@ public class GameController {
 
             // 승리 판정
             if (model.isCurrentPlayerWinner()) {
-                JOptionPane.showMessageDialog(
+                int choice = JOptionPane.showConfirmDialog(
                     view,
-                    model.getCurrentPlayer().getName() + "님이 승리했습니다!",
+                    model.getCurrentPlayer().getName() + "님이 승리했습니다!\n 다시 시작하시겠습니까?",
                     "Game Over",
+                    JOptionPane.YES_NO_OPTION,
                     JOptionPane.INFORMATION_MESSAGE
                 );
+                
+                if (choice == JOptionPane.YES_OPTION) {
+                	// 기존 창 닫기
+                	SwingUtilities.getWindowAncestor(view).dispose();
+                	
+                	// 게임 다시 시작하기
+                	new SettingController();
+                }
+                else {
+                	System.exit(0);
+                }
                 return;
             }
 
